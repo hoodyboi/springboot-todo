76226e0c146309057bbcf4d1d45d312a6a888b0e
hoodyboi <hoodyboi@naver.com>
2025-06-16 19:14:17 +0900

    fear: UserControllerTest에 사용자 초기화 및 User 엔티티 import 수정

--- src/test/java/com/example/springstart/controller/UserControllerTest.java
+++ src/test/java/com/example/springstart/controller/UserControllerTest.java
@@ -1,12 +1,16 @@
 package com.example.springstart.controller;
 
+import com.example.springstart.domain.User;
 import com.example.springstart.dto.LoginRequestDto;
+import com.example.springstart.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,6 +25,17 @@ class UserControllerTest {
 
     @Autowired MockMvc mvc;
     @Autowired ObjectMapper om;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    void setUp(){
+        String encodedPassword = passwordEncoder.encode("1234");
+        User user = new User("jm4", encodedPassword, "ROLE_USER");
+        userRepository.save(user);
+    }
 
     @Test
     void login_success_returns_tokens() throws Exception {
