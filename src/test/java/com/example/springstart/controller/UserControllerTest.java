ed25db65c47d10d4ce5557cc58cfaad091ae6c4e
hoodyboi <hoodyboi@naver.com>
2025-06-16 11:39:59 +0900

    fix(ci): H2 configuration for tests

--- src/test/java/com/example/springstart/controller/UserControllerTest.java
+++ src/test/java/com/example/springstart/controller/UserControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -15,6 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class UserControllerTest {
 
     @Autowired MockMvc mvc;
