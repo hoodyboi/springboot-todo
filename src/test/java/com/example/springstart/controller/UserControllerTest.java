b870794ea16ed4d0eea1f62436969f2e2bf3c2dc
hoodyboi <hoodyboi@naver.com>
2025-06-16 21:35:14 +0900

    fix: UserControllerTest에 @Transactional 어노테이션 추가

--- src/test/java/com/example/springstart/controller/UserControllerTest.java
+++ src/test/java/com/example/springstart/controller/UserControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@Transactional
 class UserControllerTest {
 
     @Autowired MockMvc mvc;
